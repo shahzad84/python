1f2742c1a14fd16002eb638825a0de3c78aebb15
hilarious3 <shahzad20022002@gmail.com>
2023-08-27 22:50:31 +0530

    occurance of character in string

---
+++ occurance_character_string.py
@@ -0,0 +1,7 @@
+string=input("please enter string: ")
+char=input("please enter character: ")
+count=0
+for i in range(len(string)):
+    if(string[i]==char):
+        count=count+1
+print("total no of occurence of ",char,"is: ",count)
