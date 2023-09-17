f85e7baebdd41c54e903b92f729baaf02ea9d299
hilarious3 <shahzad20022002@gmail.com>
2023-09-18 02:12:30 +0530

    Python program to find sum of integers present in the string

---
+++ sum_integer_in_string.py
@@ -0,0 +1,8 @@
+# Python program to find sum of integers present in the string
+
+string=input("enter a string")
+sum=0
+for i in string:
+    if i.isdigit():
+        sum=sum+int(i)
+print("sum = ",sum)
