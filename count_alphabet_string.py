ec600addd87f4be4612ba695d161b570b30ee709
hilarious3 <shahzad20022002@gmail.com>
2023-09-18 01:22:30 +0530

    Algorithm to count alphabets, digits, and special characters in the string

---
+++ count_alphabet_string.py
@@ -0,0 +1,16 @@
+# Algorithm to count alphabets, digits, and special characters in the string
+
+
+string = input("Enter a String : ")
+alphabets=0
+digits=0
+specialChars=0
+#checks for each character in the string
+for i in string: 
+    if i.isalpha():
+        alphabets+=1
+    elif i.isdigit():
+        digits+=1
+    else:
+        specialChars+=1
+print("alphabets =",alphabets,"digits =",digits,"specialChars =",specialChars)
