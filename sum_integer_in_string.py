36734522a0db0b45ac63030d1a82135770bdac12
hilarious3 <shahzad20022002@gmail.com>
2023-10-05 19:43:24 +0530

    sum of integer string revision

--- sum_integer_in_string.py
+++ sum_integer_in_string.py
@@ -1,8 +1,17 @@
 # Python program to find sum of integers present in the string
 
-string=input("enter a string")
+# string=input("enter a string")
+# sum=0
+# for i in string:
+#     if i.isdigit():
+#         sum=sum+int(i)
+# print("sum = ",sum)
+
+
+string=input("enter a string: ")
 sum=0
 for i in string:
     if i.isdigit():
         sum=sum+int(i)
-print("sum = ",sum)
+print(sum)
+
