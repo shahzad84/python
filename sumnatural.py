681f45e17e45d8b542413a13e97a531cbb6bd692
hilarious3 <shahzad20022002@gmail.com>
2023-10-05 10:04:41 +0530

    sumnatural rev

--- sumnatural.py
+++ sumnatural.py
@@ -1,6 +1,17 @@
+# num=5
+# if num<0:
+#     print("enter more than 0")
+# else:
+#     sum=0
+#     while num>0:
+#         sum+=num
+#         num-=1
+#     print(sum)
+
+
 num=5
 if num<0:
-    print("enter more than 0")
+    print("enter no. more than 0") 
 else:
     sum=0
     while num>0:
