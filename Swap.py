cfe6a85b22d9b29f57d31977af984b4028e4d4a6
hilarious3 <shahzad20022002@gmail.com>
2023-10-05 09:37:37 +0530

    swap rev 1

--- Swap.py
+++ Swap.py
@@ -1,7 +1,17 @@
+# a=4
+# b=5
+# x=a
+# a=b
+# b=x
+# print("the value of a is : ",a)
+# print("the value of b is : ",b)
+
+
 a=4
 b=5
 x=a
 a=b
 b=x
-print("the value of a is : ",a)
-print("the value of b is : ",b)
+
+print("the value of a after swap is :", a)
+print("the value of b after swap is :", b)
