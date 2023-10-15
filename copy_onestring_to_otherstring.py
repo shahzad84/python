7748343b0f933d97d860a1348c1aecd44383c75a
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 11:38:38 +0530

    copyonestring

--- copy_onestring_to_otherstring.py
+++ copy_onestring_to_otherstring.py
@@ -1,8 +1,21 @@
 # Python Program to Copy one String to Another String
 
 
-string=input("enter a string: ")
-new_string=""
-for char in string:
-    new_string+=char
-print("new string: ",new_string)
+# string=input("enter a string: ")
+# new_string=""
+# for char in string:
+#     new_string+=char
+# print("new string: ",new_string)
+
+# string="good si what" 
+# new=""
+# for i in string:
+#     new+=i
+# print(new)
+
+
+string="good"
+new=" "
+for i in string:
+    new+=i
+print(new)
