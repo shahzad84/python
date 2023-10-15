bffba39c77ff556c7ba0117cc5a307697e9e6aa3
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 11:51:25 +0530

    countoccurance

--- count_occurance_character_string.py
+++ count_occurance_character_string.py
@@ -1,9 +1,27 @@
 # Python Program to count occurrence of characters in string
 
-string=input("please enter string: ")
-char=input("please enter character: ")
+# string=input("please enter string: ")
+# char=input("please enter character: ")
+# count=0
+# for i in range(len(string)):
+#     if(string[i]==char):
+#         count=count+1
+# print("total no of occurence of ",char,"is: ",count)
+
+
+# string="good"
+# char="o"
+# count=0
+# for i in range(len(string)):
+#     if(string[i]==char):
+#         count=count+1
+# print(count)
+
+
+string="good"
+char="o"
 count=0
 for i in range(len(string)):
     if(string[i]==char):
-        count=count+1
-print("total no of occurence of ",char,"is: ",count)
+        count+=1
+print(count)
