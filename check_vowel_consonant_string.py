05a9dc80cbe308f1812f8b27756aa34e53a717d5
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 10:04:28 +0530

    vowel.

--- check_vowel_consonant_string.py
+++ check_vowel_consonant_string.py
@@ -1,8 +1,13 @@
 # Python program to check given character is vowel or consonant.
 
-ch=input("Enter a character: ")
-if(ch=="a",ch=="e",ch=="i",ch=="o",ch=="u"):
+# ch=input("Enter a character: ")
+# if(ch=="a",ch=="e",ch=="i",ch=="o",ch=="u"):
+#     print("it is vowel")
+# else:
+#     print("it is consonant")
+
+ch = input("Enter a character: ")
+if (ch=="a",ch=="e",ch=="i",ch=="o",ch=="u"):
     print("it is vowel")
 else:
-    print("it is consonant")
-
+    print("it is not vowel")
