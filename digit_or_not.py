30a2d09c1cd4b3c58188a51030e173c40d87f507
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 12:03:41 +0530

    digit!

--- digit_or_not.py
+++ digit_or_not.py
@@ -1,6 +1,20 @@
 # Python program to check given input is digit or not
-a=input("Enter a no.")
+# a=input("Enter a no.")
+# if a>="0" and a<="9":
+#     print("it is digit",a)
+# else:
+#     print("it is not digit",a)
+
+
+# a="9"
+# if a >="0" and a<="9":
+#     print("it is digit",a)
+# else:
+#     print("it not")
+
+
+a="-1"
 if a>="0" and a<="9":
-    print("it is digit",a)
+    print("digit")
 else:
-    print("it is not digit",a)
+    print("not digit")
