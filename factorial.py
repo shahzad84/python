22039e05343c0a8ecba6f3d68cded0d93e26faf9
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 12:13:18 +0530

    factorial

--- factorial.py
+++ factorial.py
@@ -1,8 +1,35 @@
+# def fact(a):
+#     if a==0:
+#         return 1
+#     else:
+#         return ((a)*fact(a-1))
+# num=4
+# result=fact(num)
+# print(result)
+
+
+# def fact(a):
+#     if a==0:
+#         return 1
+#     else:
+#         return ((a)*fact(a-1))
+# num=4
+# result=fact(num)
+# print(result)
+
+# def fact(a):
+#     if a==0:
+#         return 1
+#     else:
+#         return ((a)*fact(a-1))
+# result=fact(4)
+# print(result)
+
 def fact(a):
     if a==0:
         return 1
     else:
         return ((a)*fact(a-1))
-num=4
-result=fact(num)
-print(result)
+print(fact(4))
+
+
