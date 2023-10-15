efdd64bc0f5b218e39fda76c9668094731daa801
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 11:59:37 +0530

    countvowels

--- count_vowels_instring.py
+++ count_vowels_instring.py
@@ -1,11 +1,32 @@
 # Python program to count vowels and consonants in the string
 
-string=input("Enter a String: ")
-vowels=0
-consonants=0
-for i in string:
-    if i in ("a","e","i","o","u"):
-        vowels+=1
+# string=input("Enter a String: ")
+# vowels=0
+# consonants=0
+# for i in string:
+#     if i in ("a","e","i","o","u"):
+#         vowels+=1
+#     elif i.isalpha():
+#         consonants+=1
+# print("vowels: ",vowels,"consonants: ",consonants)
+
+
+# string="ggod"
+# vowels=0
+# consonant=0
+# for i in string:
+#     if i in ("a","e","i","o","u"):
+#         vowels+=1
+#     elif i.isalpha():
+#         consonant+=1
+# print(vowels,consonant)
+
+sting="good"
+vowel=0
+const=0
+for i in sting:
+    if i in ("a","i","o","u"):
+        vowel+=1
     elif i.isalpha():
-        consonants+=1
-print("vowels: ",vowels,"consonants: ",consonants)
+        const+=1
+print(vowel,const)
