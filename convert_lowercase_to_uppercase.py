9d963fbf5ac832ba46d81dc6d841d2288ffc3890
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 10:19:21 +0530

    lowertoupper

--- convert_lowercase_to_uppercase.py
+++ convert_lowercase_to_uppercase.py
@@ -1,5 +1,38 @@
 # Python Program to Convert lowercase to uppercase character
-string=input("Enter a string")
+# string=input("Enter a string")
+# result=" "
+# for i in string:
+#     if i.islower():
+#         i=i.upper()
+#     result+=i
+# print(result)
+
+# string="hii"
+# result=''
+# for i in string:
+#     if i.islower():
+#         i=i.upper()
+#     result+=i
+# print(result)
+
+# string="hii"
+# result=" "
+# for i in string:
+#     if i.islower():
+#         i=i.upper()
+#     result+=i
+# print(result)
+
+# string="hii"
+# result=" "
+# for i in string:
+#     if i.islower():
+#         i=i.upper()
+#     result+=i
+# print(result)
+
+
+string="hii"
 result=" "
 for i in string:
     if i.islower():
