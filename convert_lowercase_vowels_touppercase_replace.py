2f4270e5978e16d27cd59cf31cad3150d8217b34
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 11:35:49 +0530

    vowelto upper

--- convert_lowercase_vowels_touppercase_replace.py
+++ convert_lowercase_vowels_touppercase_replace.py
@@ -1,8 +1,36 @@
 # Convert Lowercase vowels to Uppercase using replace() method
-string=input("Enter a string: ")
-vowels="aeiou"
-for char in string:
-    if char in vowels:
-        upper_char=char.upper()
-        string=string.replace(char,upper_char)
-print(string)
+# string=input("Enter a string: ")
+# vowels="aeiou"
+# for char in string:
+#     if char in vowels:
+#         upper_char=char.upper()
+#         string=string.replace(char,upper_char)
+# print(string)
+
+
+# string="good"
+# vowels="aeiou"
+# for char in string:
+#     if char in vowels:
+#         upper=char.upper()
+#         string=string.replace(char,upper)
+# print(string)
+
+
+# string="good"
+# vowel='aeiou'
+# for i in string:
+#     if i in vowel:
+#         upper=i.upper()
+#         string=string.replace(i,upper)
+# print(string)
+
+
+# string="good"
+# vowel="aeiou"
+# for i in string:
+#     if i in vowel:
+#         upper=i.upper()
+#         string=string.replace(i,upper)
+# print(string)
+
