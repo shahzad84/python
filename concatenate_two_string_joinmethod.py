c3d442b65653ed75c75f61c17f615bccd35b4e15
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 10:06:06 +0530

    concatenate

--- concatenate_two_string_joinmethod.py
+++ concatenate_two_string_joinmethod.py
@@ -1,4 +1,9 @@
 # Python program to concatenate String using join() method
-a="hi"
-b="me"
-print("concatenate","".join([a,b]))
+# a="hi"
+# b="me"
+# print("concatenate","".join([a,b]))
+
+
+a="hii"
+b="good"
+print(" ".join([a,b]))
