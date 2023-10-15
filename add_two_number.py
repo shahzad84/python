d5c8adf70e91a102f5c71b27e033e609517316d8
hilarious3 <shahzad20022002@gmail.com>
2023-10-15 09:58:00 +0530

    adding.

--- add_two_number.py
+++ add_two_number.py
@@ -1,4 +1,11 @@
-a=float(input("Enter the first number: "))
-b=float(input("Enter the second number: "))
-sum=a+b
-print("the sum of a and b : ",sum)
+# a=float(input("Enter the first number: "))
+# b=float(input("Enter the second number: "))
+# sum=a+b
+# print("the sum of a and b : ",sum)
+
+
+# a=float(input('Enter a number'))
+# b=float(input("Enter second number"))
+# sum =a+b
+# print("the sum of a and b is ", sum)
+
