123e129c1bebdef8ee525388a68afaa925978e71
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 10:28:26 +0530

    which is greater

--- largest3.py
+++ largest3.py
@@ -1,10 +1,21 @@
-a=4
-b=5
-c=6
-if a>c and a>b:
-    print(a,"is largest")
-if b>a and b>c:
-    print(b,"is largest")
-else:
-    print(c,"is largest")
+# a=4
+# b=5
+# c=6
+# if a>c and a>b:
+#     print(a,"is largest")
+# if b>a and b>c:
+#     print(b,"is largest")
+# else:
+#     print(c,"is largest")
     
+
+
+a=2
+b=5
+c=9
+if a>=b and a>=c:
+    print(a," is greater")
+elif b>=a and b>=c:
+    print(b,"is greater")
+else:
+    print(c,"is grater")
