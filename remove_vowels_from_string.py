1c79f41ccbd2d145e052b104710934bf2c698660
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 12:57:26 +0530

    remove vowel

--- remove_vowels_from_string.py
+++ remove_vowels_from_string.py
@@ -1,8 +1,28 @@
 # Python code to remove vowels from the string
-string=input("Enter a string: ")
-result=" "
-for i in string: 
-    if i in ("a","e","i","o","u"):
-        i=" "
+# string=input("Enter a string: ")
+# result=" "
+# for i in string: 
+#     if i in ("a","e","i","o","u"):
+#         i=" " #If i is one of the specified vowels, this line sets the value of i to an empty string
+#     result+=i
+# print(result)
+
+
+# string="hii this"
+# res=""
+# for i in string:
+#     if i in ("a","i","o","u"):
+#         i=""
+#     res+=i
+# print(res)
+
+string="hii"
+result=""
+for i in string:
+    if i in ("aeiou"):
+        i=""
     result+=i
 print(result)
+
+
+
