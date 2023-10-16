b0273dd7fce73e2a1affc7634fdcf33ca6a5f7b6
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 12:42:41 +0530

    remove repeated char

--- remove_repeated_character_string.py
+++ remove_repeated_character_string.py
@@ -1,9 +1,50 @@
 # Python Program to Remove Repeated Character from String
-def remove_duplicate(s):
-    unique_chars=[]
-    for char in s:
-        if char not in unique_chars:
-            unique_chars.append(char)
-    return "".join(unique_chars)
-string=input("enter a string: ")
-print(remove_duplicate(string))
+# def remove_duplicate(s):
+#     unique_chars=[]
+#     for char in s:
+#         if char not in unique_chars:
+#             unique_chars.append(char)
+#     return "".join(unique_chars)
+# string=input("enter a string: ")
+# print(remove_duplicate(string))
+
+# def dup(s):
+#     unique=[]
+#     for i in s:
+#         if i not in unique:
+#             unique.append(i)
+#     return "".join(unique)
+# string="hiihiiiis"
+# print(dup(string))
+
+
+# def dup(s):
+#     unique=[]
+#     for i in s:
+#         if i not in unique:
+#             unique.append(i)
+#     return "".join(unique)
+# s="jii"
+# print(dup(s))
+
+
+# def dup(s):
+#     unique=[]
+#     for i in s:
+#         if i not in unique:
+#             unique.append(i)
+#     return "".join(unique)
+# a="hii"
+# print(dup(a))
+
+def dup(s):
+    unique=[]
+    for i in s:
+        if i not in unique:
+            unique.append(i)
+    return "".join(unique)
+a="hii"
+print(dup(a))
+
+
+
