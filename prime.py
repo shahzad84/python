ee4def1de7449e2e8b10a21d0f22b0bf677b7fa2
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 11:22:01 +0530

    prime

--- prime.py
+++ prime.py
@@ -1,10 +1,46 @@
-a=4
+# a=4
+# if a==1:
+#     print("not prime")
+# if a>1:
+#     for i in range(2,a):
+#         if a% i==0:
+#             print("not prime")
+#             break
+#     else:
+#         print("prime")
+
+# a=4
+# if a==1:
+#     print("not prime")
+# if a>1:
+#     for i in range(2,a):
+#         if a%i==0:
+#             print("not prime")
+#             break
+#     else:
+#         print("prime")
+
+
+# a=3
+# if a==1:
+#     print("not prime")   
+# if a>1:
+#     for i in range(2,a):
+#         if a%i==0:
+#             print("not prime")
+#             break
+#     else:
+#         print("prime")
+
+
+a=3
 if a==1:
     print("not prime")
 if a>1:
     for i in range(2,a):
-        if a% i==0:
+        if a%i==0:
             print("not prime")
             break
     else:
         print("prime")
+        
