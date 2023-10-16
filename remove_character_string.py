b52ee672c3e3a1b8691e50177026aec566439644
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 12:13:15 +0530

    remove character in string

--- remove_character_string.py
+++ remove_character_string.py
@@ -1,10 +1,17 @@
 # Python program to remove given character from String.
-
-str=input("")
-hi=input("")
-print(str.replace(hi," "))
-
-
 # ex:str= i am shahzad
 #     ch=a
 #     output:i m sh h z d
+# str=input("")
+# hi=input("")
+# print(str.replace(hi," "))
+
+
+# a="hii this"
+# b="i"
+# print(a.replace(b," "))
+
+
+a="this is"
+b="i"
+print(a.replace(b," "))
