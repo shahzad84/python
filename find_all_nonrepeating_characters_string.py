0fc283118e2609a2fe3ebd5d976edf79c6096358
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 10:23:47 +0530

    findnonrepeating

--- find_all_nonrepeating_characters_string.py
+++ find_all_nonrepeating_characters_string.py
@@ -1,14 +1,55 @@
 # Python program to find all non repeating characters in the string
-string = input('Please enter a string: ')
-freq_dict = {}
-for char in string:
-    if char in freq_dict:
-        freq_dict[char] += 1
-    else:
-        freq_dict[char] = 1
-non_repeating_chars = ""
-for char in string:
-    if freq_dict[char] == 1:
-        non_repeating_chars += char
-print("Non-repeating characters:", non_repeating_chars)
+# string = input('Please enter a string: ')
+# freq_dict = {}
+# for char in string:
+#     if char in freq_dict:
+#         freq_dict[char] += 1
+#         print(freq_dict)
+#     else:
+#         freq_dict[char] = 1
+# non_repeating_chars = ""
+# for char in string:
+#     if freq_dict[char] == 1:
+#         non_repeating_chars += char
+# print("Non-repeating characters:", non_repeating_chars)
 
+# string="hiig"
+# dic={}
+# for i in string:
+#     if i in dic:
+#         dic[i]+=1
+#     else:
+#         dic[i]=1
+# non_rep=""
+# for i in string:
+#     if dic[i]==1:
+#         non_rep+=i
+# print(non_rep)
+
+
+# string="hii"
+# dic={}
+# for i in string:
+#     if i in dic:
+#         dic[i]+=1
+#     else:
+#         dic[i]=1
+# non_rep=""
+# for i in string:
+#     if dic[i]==1:
+#         non_rep+=i
+# print(non_rep)
+
+
+string="ihii"
+dic={}
+for i in string:
+    if i in dic:
+        dic[i]+=1
+    else:
+        dic[i]=1
+non_rep=""
+for i in string:
+    if dic[i]==1:
+        non_rep+=i
+print(non_rep)
