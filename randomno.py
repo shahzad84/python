b5e670cd42b25ce446273202c9df819a48170360
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 11:49:36 +0530

    random

--- randomno.py
+++ randomno.py
@@ -1,3 +1,11 @@
+# import random
+# a=random.randint(2,9)
+# print(a)
+
+# import random
+# a=random.randint(2,9)
+# print(a)
+
 import random
-a=random.randint(2,9)
+a=random.randint(2,8)
 print(a)
