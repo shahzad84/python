ed9a3604e19f049794614f6b5b0c43cbb2a420f3
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 11:06:36 +0530

    oddeven

--- odd_even.py
+++ odd_even.py
@@ -1,4 +1,18 @@
-a=float(input("Enter the first number: "))
+# a=float(input("Enter the first number: "))
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+# a=4
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+a=7
 if a%2==0:
     print("even")
 else:
