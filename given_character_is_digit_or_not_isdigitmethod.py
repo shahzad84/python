d481fbac7bd9365d9738605920d0fc7123fa094c
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 10:25:27 +0530

    isdigit

--- given_character_is_digit_or_not_isdigitmethod.py
+++ given_character_is_digit_or_not_isdigitmethod.py
@@ -1,7 +1,15 @@
 # Python program to check given character is digit or not using isdigit…
 
-ch=input("Enter a character")
+# ch=input("Enter a character")
+# if(ch.isdigit()):
+#     print("it is digit",ch)
+# else:
+#     print("its not digit")
+
+
+ch="3"
 if(ch.isdigit()):
-    print("it is digit",ch)
+    print("digit")
 else:
-    print("its not digit")
+    print("not digit")
+    
