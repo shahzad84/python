f4e329e48d6ff66726e29b91f363fc1348bb0c51
hilarious3 <shahzad20022002@gmail.com>
2023-10-16 12:03:28 +0530

    removespaces

--- remove_blackspaces_in_function.py
+++ remove_blackspaces_in_function.py
@@ -1,8 +1,23 @@
 # Python program to remove spaces from string without inbuilt function
-string=input("Enter a string: ")
-result=" "
+# string=input("Enter a string: ")
+# result=" "
+# for i in string:
+#     if i!=" ":
+#         result+=i
+
+# print(result)
+
+# string="hii this"
+# result=""
+# for i in string:
+#     if i!=" ": 
+#         result+=i
+# print(result)
+
+string="this is "
+a=""
 for i in string:
     if i!=" ":
-        result+=i
+        a+=i
+print(a)
 
-print(result)
