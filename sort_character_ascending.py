08f38af5d3367161533ca6663277262614a29c2b
hilarious3 <shahzad20022002@gmail.com>
2023-10-17 12:14:18 +0530

    sort character

--- sort_character_ascending.py
+++ sort_character_ascending.py
@@ -1,9 +1,20 @@
 # Python program to sort string character in ascending order
 
-string=input("enter a string")
-strList=list(string)
-sortedString="".join(sorted(strList))
-print(sortedString)
+# string=input("enter a string")
+# strList=list(string)
+# sortedString="".join(sorted(strList))
+# print(sortedString)
 
 # for descending order
 # sortedString=''.join(sorted(strList, reverse =True)) 
+
+
+# string="hiia"
+# strlist=list(string)
+# print("".join(sorted(strlist)))
+
+
+string="hiia"
+strlist=list(string)
+print("".join(sorted(strlist)))
+
