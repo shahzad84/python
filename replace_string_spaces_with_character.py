840ec0104b409272d15634f49e42851ec7900f81
hilarious3 <shahzad20022002@gmail.com>
2023-10-17 12:08:57 +0530

    repalce string spaces

--- replace_string_spaces_with_character.py
+++ replace_string_spaces_with_character.py
@@ -1,9 +1,29 @@
 # Python code to replace the string space with given character
-string=input("Enter the string")
+# string=input("Enter the string")
+# result=""
+# ch=input("enter a character")
+# for i in string:
+#     if i==" ":
+#         i=ch
+#     result +=i
+# print("sting result",result)
+
+
+# string="hii "
+# ch="shahzad"
+# result=""
+# for i in string:
+#     if i==" ":
+#         i=ch
+#     result+=i
+# print(result)
+
+
+string="hii "
+ch="shahzad"
 result=""
-ch=input("enter a character")
 for i in string:
     if i==" ":
         i=ch
-    result +=i
-print("sting result",result)
+    result+=i
+print(result)
