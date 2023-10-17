e6f844dfdba52fdb10916223e7525491060ad852
hilarious3 <shahzad20022002@gmail.com>
2023-10-17 12:10:28 +0530

    separate character

--- seprate_characters_in_string.py
+++ seprate_characters_in_string.py
@@ -1,8 +1,15 @@
 # Python Program to Separate Characters in a Given String
 
-str="hellow python"
+# str="hellow python"
+
+# for char in str:
+#     print(char)
+#     print("\n")
+    
+
+str="hellow good"
 
 for char in str:
     print(char)
     print("\n")
-    
+
