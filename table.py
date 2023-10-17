8316afd2e7b99722c515ea65870434b398e348cf
hilarious3 <shahzad20022002@gmail.com>
2023-10-17 12:29:58 +0530

    table

--- table.py
+++ table.py
@@ -3,7 +3,11 @@
 #     print(n,"*",i,"=",n*i)
 
 
-n=7
-for i in range (1,11):
-    print(n,"*",i,"=",n*i)
+# n=7
+# for i in range (1,11):
+#     print(n,"*",i,"=",n*i)
     
+
+n=7
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
