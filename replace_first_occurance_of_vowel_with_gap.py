38e47eb91c89df054549bbfe95478340528cdbd3
hilarious3 <shahzad20022002@gmail.com>
2023-10-17 12:02:53 +0530

    repalce -

--- replace_first_occurance_of_vowel_with_gap.py
+++ replace_first_occurance_of_vowel_with_gap.py
@@ -1,13 +1,35 @@
 # Program to Replace first occurrence of Vowel with ‘-‘ in String
-def replace_vowel(string):
-    vowels="aeiouAIOUE"
-    for i in range(len(string)):
-        if string[i] in vowels:
-            string=string[:i]+"-"+string[i+1:]
+# def replace_vowel(string):
+#     vowels="aeiouAIOUE"
+#     for i in range(len(string)):
+#         if string[i] in vowels:
+#             string=string[:i]+"-"+string[i+1:]
+#             break
+#     return string
+
+
+# string=input("ente a string : ")
+# print("original string : ",string)
+# print("modified string: ", replace_vowel(string))
+
+# def replace(string):
+#     vowels="aeiou"
+#     for i in range(len(string)):
+#         if string[i] in vowels:
+#             string=string[:i]+"-"+string[i+1:]
+#             break
+#     return string
+# string="hii"
+# print(replace(string))
+
+
+def replace(s):
+    vowel="aeiou"
+    for i in range(len(s)):
+        if s[i] in vowel:
+            s=s[:i]+"-"+s[i+1:]
             break
-    return string
+    return s
+s="hii"
+print(replace(s))
 
-
-string=input("ente a string : ")
-print("original string : ",string)
-print("modified string: ", replace_vowel(string))
