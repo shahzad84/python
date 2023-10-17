c5d28856bbc2a7d6676a5e4f9185781b4c54158a
hilarious3 <shahzad20022002@gmail.com>
2023-10-17 12:17:20 +0530

    sum integer in string

--- sum_integer_in_string.py
+++ sum_integer_in_string.py
@@ -8,10 +8,24 @@
 # print("sum = ",sum)
 
 
-string=input("enter a string: ")
+# string=input("enter a string: ")
+# sum=0
+# for i in string:
+#     if i.isdigit():
+#         sum=sum+int(i)
+# print(sum)
+
+# string="hii 3 5"
+# sum=0
+# for i in string:
+#     if i.isdigit():
+#         sum=sum+int(i)
+# print(sum)
+
+
+string="hii 4 3"
 sum=0
 for i in string:
     if i.isdigit():
-        sum=sum+int(i)
+        sum+=int(i)
 print(sum)
-
