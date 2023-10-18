8f42e0eae6ff2791f60e91322623124fcf4edb67
hilarious3 <shahzad20022002@gmail.com>
2023-10-18 23:51:39 +0530

    missing no

---
+++ Array/missingno_inrange.py
@@ -0,0 +1,12 @@
+# Python Program to find missing number in array
+
+arr=[]
+n = int(input("enter size of array : "))
+for x in range(n-1):
+    x=int(input("enter element of array : "))
+    arr.append(x)
+sum = (n*(n+1))/2;
+sumArr = 0
+for i in range(n-1):
+    sumArr = sumArr+arr[i];
+print(int(sum-sumArr)) 
